388f102056d6cab071b54ae4b19e0adf8f73d97b
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-05-09 21:58:43 -0300

    feat: switch AI provider from OpenAI to Groq

--- app/api/example/route.ts
+++ app/api/example/route.ts
@@ -1,5 +1,23 @@
-import { NextResponse } from "next/server"
+import { createGroq } from "@ai-sdk/groq"; // Importação do pacote
+import { generateText } from "ai";
+import { NextResponse } from "next/server";
 
-export async function POST(request: Request){
-    return NextResponse.json({message: 'Hello, word!'})
-};
+const groq = createGroq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+export async function POST(request: Request) {
+  try {
+    const result = await generateText({
+     
+      model: groq('llama-3.3-70b-versatile'), 
+      system: "Você é um assistente de IA humorístico",
+      prompt: 'Quanto que é 2+2'
+    });
+
+    return NextResponse.json({ message: result.text });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Erro na IA" }, { status: 500 });
+  }
+}
